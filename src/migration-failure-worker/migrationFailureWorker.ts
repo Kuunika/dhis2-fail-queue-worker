5615e29ac7f96a15ba60ca6001983dffc2bb4b02
mmalumbo <mmalumbo@gmail.com>
2019-04-16 09:12:33 +0200

    removed nested ifs.
    Signed-off-by: mmalumbo <mmalumbo@gmail.com>

--- src/migration-failure-worker/migrationFailureWorker.ts
+++ src/migration-failure-worker/migrationFailureWorker.ts
@@ -28,23 +28,21 @@ export const migrationFailureWorker = async (
 
       const { lastAttempt, attempts = 1 } = parsedMessage;
       const inWaitingPeriod = await isWaiting(config, lastAttempt);
+      const attemptsNotExhausted = checkAttemptsAvailability(config, attempts);
 
       if (!inWaitingPeriod) {
         parsedMessage.attempts = attempts + 1;
         await migrate(config, connection, worker, parsedMessage);
       }
 
-      if (inWaitingPeriod) {
-        const attemptsNotExhausted = checkAttemptsAvailability(config, attempts);
-        if (attemptsNotExhausted) {
-          await pushToFailQueue(config, worker, parsedMessage);
-        }
+      if (inWaitingPeriod && attemptsNotExhausted) {
+        await pushToFailQueue(config, worker, parsedMessage);
+      }
 
-        if (!attemptsNotExhausted) {
-          parsedMessage.migrationFailed = true;
-          parsedMessage.source = 'failqueue';
-          await pushToEmailQueue(config, worker, parsedMessage);
-        }
+      if (!attemptsNotExhausted) {
+        parsedMessage.migrationFailed = true;
+        parsedMessage.source = 'failqueue';
+        await pushToEmailQueue(config, worker, parsedMessage);
       }
     } catch (err) {
       error(err.message);
